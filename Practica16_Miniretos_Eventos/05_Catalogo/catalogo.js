ff0f7714d7c92613ecc0a346c3b02f4b13563974
Marcos López González <2005052@alu.murciaeduca.es>
2025-11-23 20:33:52 +0100

    Practica16_Miniretos_Eventos TERMINADA

--- Practica16_Miniretos_Eventos/05_Catalogo/catalogo.js
+++ Practica16_Miniretos_Eventos/05_Catalogo/catalogo.js
@@ -31,13 +31,14 @@ const productos = [
     new Producto("Monitor QLED", "pantalla", 199.99, true),
     new Producto("Monitor roto", "pantalla", 0, false),
 
-    new Producto("Salchichas", "otro", 2.99, true),
+    new Producto("Salchichas", "otro", 2.99, false),
     new Producto("Tomates", "otro", 3.99, true),
     new Producto("Servilletas", "otro", 1.99, true),
 ];
 
 // Referencias al DOM: buscador, selectCategoria, checkSoloStock, contenedorTarjetas, mensajeSinResultados
 const buscador = document.getElementById("buscadorNombre");
+const selectCategoria = document.getElementById("selectCategoria");
 const checkSoloStock = document.getElementById("checkSoloStock");
 const contenedorTarjetas = document.getElementById("contenedorTarjetas");
 const mensajeSinResultados = document.getElementById("mensajeSinResultados");
@@ -48,42 +49,123 @@ function renderizarProductos(lista) {
     contenedorTarjetas.innerHTML = "";
 
     // Si la lista está vacía:
+    if (lista.length === 0) {
+        mensajeSinResultados.style.display = "block";
+        return;
+    }
     //  - mostrar el mensajeSinResultados
     //  - y salir de la función
 
     // Si hay elementos:
     //  - ocultar mensajeSinResultados
+    mensajeSinResultados.style.display = "none";
     //  - recorrer la lista y crear un div.tarjeta por cada producto
-    //  - dentro de la tarjeta, crear:
-    //      * h3 con el nombre
-    //      * span o p con la categoría (badge-categoria)
-    //      * p con el precio
-    //      * p indicando si hay stock (stock-ok o stock-no)
-    //  - añadir listeners de mouseover / mouseout si se quiere cambiar ligeramente el estilo
+    for (let i = 0; i < lista.length; i++) {
+        let div = document.createElement("div");
+        div.className = "div.tarjeta";
+        //  - dentro de la tarjeta, crear:
+        //      * h3 con el nombre
+        let h3 = document.createElement("h3");
+        h3.textContent = lista[i].nombre;
+        //      * span o p con la categoría (badge-categoria)
+        let pCategoria = document.createElement("p");
+        pCategoria.className = "badge-categoria";
+        pCategoria.textContent = lista[i].categoria;
+        //      * p con el precio
+        let pPrecio = document.createElement("p");
+        pPrecio.textContent = lista[i].precio;
+        //      * p indicando si hay stock (stock-ok o stock-no)
+        let pStock = document.createElement("p");
+        pStock.textContent = lista[i].stock;
+
+        div.appendChild(h3);
+        div.appendChild(pCategoria);
+        div.appendChild(pPrecio);
+        div.appendChild(pStock);
+
+        contenedorTarjetas.appendChild(div);
+
+        //  - añadir listeners de mouseover / mouseout si se quiere cambiar ligeramente el estilo
+    }
 }
 
 // Función que aplica los filtros actuales al array de productos
 function obtenerProductosFiltrados() {
     // Leer el texto del buscador, la categoría seleccionada y el checkbox de stock
+    let textoBuscador = buscador.value.trim().toLowerCase();
+    // console.log(textoBuscador);
+    let categoria = selectCategoria.value;
+    let stock = checkSoloStock.checked;
+    // console.log(textoBuscador, categoria, stock);
 
     // Crear un nuevo array filtrado a partir de productos:
+    listaFiltrada = [];
     //  - por nombre: incluye si contiene el texto del buscador (toLowerCase)
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre.trim().toLowerCase().includes(textoBuscador))
+            listaFiltrada.push(productos[i]);
+    }
+    // console.log(listaFiltrada.length, listaFiltrada);
     //  - por categoría: si selectCategoria no es "todos", filtra por esa categoría
+    if (categoria !== "todos") {
+        indicesABorrar = [];
+        for (let i = 0; i < listaFiltrada.length; i++) {
+            if (listaFiltrada[i].categoria !== categoria)
+                indicesABorrar.unshift(i);
+            // listaFiltrada.splice(i, 1);
+        }
+
+        for (let i = 0; i < indicesABorrar.length; i++) {
+            listaFiltrada.splice(indicesABorrar[i], 1);
+        }
+    }
+    // console.log(listaFiltrada.length, listaFiltrada);
     //  - por stock: si checkSoloStock está marcado, filtra para que solo aparezcan los que tienen stock true
+    listaFiltradaStock = [];
+    if (stock) {
+        indicesABorrar = [];
+        for (let i = 0; i < listaFiltrada.length; i++) {
+            if (!listaFiltrada[i].stock) {
+                indicesABorrar.unshift(i);
+            }
+            // listaFiltrada.splice(i, 1);
+        }
+
+        for (let i = 0; i < indicesABorrar.length; i++) {
+            listaFiltrada.splice(indicesABorrar[i], 1);
+        }
+    }
 
     // Devolver el array filtrado
+    // console.log(listaFiltrada.length, listaFiltrada);
+    return listaFiltrada;
 }
 
 // Función principal que actualiza la vista según los filtros
 function actualizarVista() {
     // Llama a obtenerProductosFiltrados()
     // Llama a renderizarProductos() con el resultado
+    // obtenerProductosFiltrados();
+    let listaFiltrada = obtenerProductosFiltrados();
+    renderizarProductos(listaFiltrada);
 }
 
 // Eventos:
 // - input en el buscador (para filtrar en tiempo real)
+buscador.addEventListener("input", actualizarVista);
 // - change en el selectCategoria
+selectCategoria.addEventListener("change", actualizarVista);
 // - change en el checkSoloStock
+checkSoloStock.addEventListener("change", function () {
+    // if (this.checked) {
+    //     console.log("checked");
+    // } else {
+    //     console.log("NO");
+    // }
+    actualizarVista();
+});
+
+actualizarVista();
 
 // En cada evento, llamar a actualizarVista()
 
